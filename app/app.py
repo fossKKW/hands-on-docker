b8f768d029d5cb0f0ab4b60dbd553a39242aff59
Tanmay Gaikwad <gaikwadtanmay723@gmail.com>
2025-01-14 01:31:16 +0530

    Implement database connection creation, task creation and deletion functions

--- app/app.py
+++ app/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for
+from storage.database import create_task, delete_task
 
 app = Flask(__name__)
 
@@ -12,14 +13,20 @@ def home():
 @app.route('/add', methods=['POST'])
 def add_task():
     task = request.form.get('task')
+    # if task:
+    #     tasks.append(task)
     if task:
-        tasks.append(task)
+        new_task = create_task(task)
+        tasks.append([new_task["id"], new_task["task"]])
     return redirect(url_for('home'))
 
 @app.route('/delete/<int:task_id>')
-def delete_task(task_id):
-    if 0 <= task_id < len(tasks):
-        tasks.pop(task_id)
+def delete_task(task_id:int):
+    # if 0 <= task_id < len(tasks):
+    #     tasks.pop(task_id)
+    deleted_task = delete_task(task_id)
+    print(deleted_task)
+    tasks.pop(task_id)
     return redirect(url_for('home'))
 
 if __name__ == '__main__':
---
+++ app/storage/database.py
@@ -0,0 +1,68 @@
+import psycopg2
+from psycopg2.extras import RealDictCursor
+
+def get_db_connection():
+    try: 
+        connection = psycopg2.connect(host="localhost", database="docker", user="postgres", password="0104", cursor_factory=RealDictCursor)
+        cur = connection.cursor()
+        print(f"Database connected")
+        cur.execute("""
+            SELECT EXISTS (
+                SELECT FROM information_schema.tables 
+                WHERE table_name = 'todo'
+            );
+        """)
+        table_exists = cur.fetchone()
+        if not table_exists:
+            cur.execute("""
+                CREATE TABLE todo (
+                    id SERIAL PRIMARY KEY,
+                    task VARCHAR(255) NOT NULL
+                );
+            """)
+            print("Table 'todo' created successfully.")
+        else:
+            print("Table 'todo' already exists.")
+        connection.commit()
+        cur.close()
+        return connection
+    except Exception as error:
+        print(f"Couldn't connect to database the error occured was {error}")
+        return None
+
+
+def create_task(task:str):
+    conn = get_db_connection()
+    if conn:
+        cur = conn.cursor()
+        try:
+            cur.execute("""INSERT INTO public.todo (task) VALUES (%s) RETURNING *;""", (task,))
+            new_task = cur.fetchone()
+            conn.commit()
+            return new_task
+        except Exception as error:
+            print(f"Couldn't add new task to the database, following  error {error} occured.")
+            return None
+        finally:
+            cur.close()
+            conn.close()
+
+
+def delete_task(id:int):
+    conn = get_db_connection()
+    if conn:
+        cur = conn.cursor()
+        try:
+            cur.execute("""DELETE FROM public.todo WHERE id=%s RETURNING *;""", (str(id), ))
+            deleted_task = cur.fetchone()
+            conn.commit()
+            return deleted_task
+        except Exception as error:
+            print(f"Couldn't delete task with id {id}, following error occured: {error}")
+            return None
+        finally:
+            cur.close()
+            conn.close()
+
+
+
